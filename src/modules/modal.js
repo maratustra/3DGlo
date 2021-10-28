2d44eeecd78d3c558440424575767e97df733487
Alena Maramygina <maratustraa@gmail.com>
2021-10-28 17:04:56 +0500

    feature(getModalOpacityEffect): добавлена функция анимиров

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,31 +1,34 @@
 const modal = () => {
-  window.addEventListener('resize', () => {
-    // let intViewportWidth = window.innerWidth;
-    // console.log(`вот это отображается ${intViewportWidth}`);
+  //window.addEventListener('resize', () => {
+  const modal = document.querySelector('.popup');
+  const buttons = document.querySelectorAll('.popup-btn');
+  const closeBtn = modal.querySelector('.popup-close');
+  const screenWidth = document.documentElement.clientWidth;
+  let opacityCounter = 0;
 
-    const modal = document.querySelector('.popup');
-    const buttons = document.querySelectorAll('.popup-btn');
-    const closeBtn = modal.querySelector('.popup-close');
-    const screenWidth = document.documentElement.clientWidth;
-    console.log(`Нет, вот это ${screenWidth}`);
+  modal.style.opacity = 0;
 
-    const modalAppearance = () => {
-      modal.classList.toggle('active-popup');
-    };
+  const getModalOpacityEffect = () => {
+    if (opacityCounter < 1) {
+      opacityCounter += 0.1;
+      setTimeout(getModalOpacityEffect, 100);
+    }
+    modal.style.opacity = `${opacityCounter}`;
+  };
 
-    buttons.forEach(btn => {
-      btn.addEventListener('click', () => {
-        modal.style.display = 'block';
-
-        if (screenWidth >= 768) {
-          console.log('Я тут!');
-          modalAppearance();
-        }
-      });
+  buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      modal.style.display = 'block';
+      getModalOpacityEffect();
     });
-
-    closeBtn.addEventListener('click', modalAppearance);
   });
+
+  closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+    modal.style.opacity = 0;
+  });
+
+  //});
 };
 
 
