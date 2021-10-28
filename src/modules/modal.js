59fdab7002b474c677aa18d5bec7d7a6b1847ea4
Alena Maramygina <maratustraa@gmail.com>
2021-10-28 18:56:39 +0500

    fix(window.addEventListener): исправлена ошибка в работе пр

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,34 +1,46 @@
 const modal = () => {
-  //window.addEventListener('resize', () => {
-  const modal = document.querySelector('.popup');
-  const buttons = document.querySelectorAll('.popup-btn');
-  const closeBtn = modal.querySelector('.popup-close');
-  const screenWidth = document.documentElement.clientWidth;
-  let opacityCounter = 0;
 
-  modal.style.opacity = 0;
+  const go = () => {
+    const modal = document.querySelector('.popup');
+    const buttons = document.querySelectorAll('.popup-btn');
+    const closeBtn = modal.querySelector('.popup-close');
+    let opacityCounter = 0;
+    let windowWidth = window.innerWidth;
+    console.log('width is :', windowWidth);
 
-  const getModalOpacityEffect = () => {
-    if (opacityCounter < 1) {
-      opacityCounter += 0.1;
-      setTimeout(getModalOpacityEffect, 100);
-    }
-    modal.style.opacity = `${opacityCounter}`;
+
+    const getModalOpacityEffect = () => {
+      if (opacityCounter < 1) {
+        opacityCounter += 0.1;
+        setTimeout(getModalOpacityEffect, 100);
+      }
+      modal.style.opacity = `${opacityCounter}`;
+    };
+
+    buttons.forEach(btn => {
+      btn.addEventListener('click', () => {
+
+        modal.style.opacity = 0;
+
+        if (windowWidth > 767) {
+          console.log('больше 768px');
+          modal.style.display = 'block';
+          getModalOpacityEffect();
+        } else {
+          console.log('меньше 768px');
+          modal.style.display = 'block';
+        }
+      });
+    });
+
+    closeBtn.addEventListener('click', () => {
+      modal.style.display = 'none';
+      modal.style.opacity = 0;
+    });
   };
 
-  buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-      modal.style.display = 'block';
-      getModalOpacityEffect();
-    });
-  });
-
-  closeBtn.addEventListener('click', () => {
-    modal.style.display = 'none';
-    modal.style.opacity = 0;
-  });
-
-  //});
+  window.addEventListener('resize', go);
+  window.addEventListener('DOMContentLoaded', go);
 };
 
 
