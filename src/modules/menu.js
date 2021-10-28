0246458669088a1dfbd02b75e33800654b5100d9
Alena Maramygina <maratustraa@gmail.com>
2021-10-28 23:44:14 +0500

    feat(menu.js): добавлена функция smoothScrollMenu и переменная scrollBtn для плавного скролла

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,14 +3,39 @@ const menu = () => {
   const menu = document.querySelector('menu');
   const closeBtn = menu.querySelector('.close-btn');
   const menuItems = menu.querySelectorAll('ul>li>a');
+  const scrollBtn = document.querySelector("a[href='#service-block']");
 
   const handleMenu = () => {
     menu.classList.toggle('active-menu');
   };
 
+  const smoothScrollMenu = (blockID) => {
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  };
+
   menuBtn.addEventListener('click', handleMenu);
   closeBtn.addEventListener('click', handleMenu);
-  menuItems.forEach(menuItem => menuItem.addEventListener('click', handleMenu));
+
+  menuItems.forEach(menuItem => {
+    menuItem.addEventListener('click', (e) => {
+      handleMenu();
+      e.preventDefault();
+
+      const blockID = e.target.getAttribute('href').substr(1);
+      smoothScrollMenu(blockID);
+    });
+  });
+
+  scrollBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const blockID = e.target.parentNode.getAttribute('href').substr(1);
+    smoothScrollMenu(blockID);
+  });
 };
 
 export default menu;
