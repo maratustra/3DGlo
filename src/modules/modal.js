514ecb6486c6bafb8b212551d0cdf060993abb4a
Alena Maramygina <maratustraa@gmail.com>
2021-10-29 14:01:59 +0500

    fix(modal.js): убрала прослушиватели window('resize') и window('DOM'), добавила в условия if-else свойство clientWidth

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,45 +1,35 @@
 const modal = () => {
 
-  const go = () => {
-    const modal = document.querySelector('.popup');
-    const buttons = document.querySelectorAll('.popup-btn');
-    const closeBtn = modal.querySelector('.popup-close');
-    let opacityCounter = 0;
-    let windowWidth = document.documentElement.clientWidth;
+  const modal = document.querySelector('.popup');
+  const buttons = document.querySelectorAll('.popup-btn');
+  const closeBtn = modal.querySelector('.popup-close');
 
-
-    const getModalOpacityEffect = () => {
-      if (opacityCounter < 1) {
-        opacityCounter += 0.1;
-        setTimeout(getModalOpacityEffect, 100);
-      }
-      modal.style.opacity = `${opacityCounter}`;
-    };
-
-    buttons.forEach(btn => {
-      btn.addEventListener('click', () => {
-
-        modal.style.opacity = 0;
-
-        if (windowWidth < 768) {
-          modal.style.display = 'block';
-        } else {
-          modal.style.display = 'block';
-          getModalOpacityEffect();
-        };
-      });
-    });
-
-    closeBtn.addEventListener('click', () => {
-      modal.style.display = 'none';
-      modal.style.opacity = 0;
-      opacityCounter = 0;
-    });
-
+  const getModalOpacityEffect = () => {
+    if (+modal.style.opacity < 1) {
+      modal.style.opacity = Number(modal.style.opacity) + 0.1;
+      setTimeout(getModalOpacityEffect, 100);
+    }
   };
 
-  window.addEventListener('resize', go);
-  window.addEventListener('DOMContentLoaded', go);
+  buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+
+      modal.style.opacity = 0;
+
+      if (document.documentElement.clientWidth < 768) {
+        modal.style.display = 'block';
+        modal.style.opacity = 1;
+      } else {
+        modal.style.display = 'block';
+        getModalOpacityEffect();
+      }
+    });
+  });
+
+  closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+    modal.style.opacity = 0;
+  });
 };
 
 
